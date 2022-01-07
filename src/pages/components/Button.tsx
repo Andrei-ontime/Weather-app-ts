b79eba41fcb78928e88488cc2e0a1e325b2df92c
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2022-01-07 20:21:10 +0300

    form component and loginpage(test)

---
+++ src/pages/components/Button.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  outline: none;
+  width: 15.5rem;
+  height: 2.5rem;
+  border: 2px solid black;
+  border-radius: 10px;
+  color: #fff;
+  background: linear-gradient(90deg, #4b6cb7 0%, #182848 100%);
+  transition: background 2.5s;
+  &:hover {
+    transition: background 2.5s;
+    background: linear-gradient(90deg, #4b6cb7 100%, #182848 100%);
+  }
+`;
+
+const Button: FC = () => {
+  return <StyledButton>Login</StyledButton>;
+};
+
+export default Button;
